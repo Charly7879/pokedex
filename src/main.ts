f850fa81d937cbcf2def5e3213d941ae46a96955
Charly <jchar7879@hotmail.com>
2026-01-30 12:49:51 -0300

    Prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /** Prefijo global */
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
