0b3f30d2f0f4aaccc21e0631614eed030aca3203
Charly <jchar7879@hotmail.com>
2026-01-30 12:34:45 -0300

    Contenido estático

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { join } from 'node:path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    })
+  ],
   controllers: [],
   providers: [],
 })
