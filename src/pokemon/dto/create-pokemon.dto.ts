ecdb04076f9a8851560779eddecdf50eec8567ea
Charly <jchar7879@hotmail.com>
2026-01-30 20:25:10 -0300

    Recibir y validar datos

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+
+    @IsString()
+    @MinLength(3)
+    readonly name: string;
+
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    readonly no: number;
+}
